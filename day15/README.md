0e1ae82fc2a2a33560b7108f3c0a44bc1c07b197
shastrihm <hrishees@gmail.com>
2022-12-28 00:12:44 -0800

    add day 15 template

---
+++ day15/README.md
@@ -0,0 +1,9 @@
+# Day 15 
+
+Problem statement for day 15 : https://adventofcode.com/2022/day/15 
+
+ To run, cd into this directory and
+
+`go run . <input file>`
+
+where `<input file>` is either `input.txt` or `test_input.txt`
---
+++ day15/soln.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
+
+func Part_one(input_file string) int {
+    file, err := os.Open(input_file)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    // Read input line by line
+    for scanner.Scan() {
+        line := scanner.Text()
+        // code here
+    }
+    return 0
+}
+
+func Part_two(input_file string) int {
+    file, err := os.Open(input_file)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    scanner := bufio.NewScanner(file)
+    // Read input line by line
+    for scanner.Scan() {
+        line := scanner.Text()
+        // code here
+    }
+    return 0
+}
+
+func main() {
+    input := os.Args[1]
+    fmt.Println(Part_one(input))
+    fmt.Println(Part_two(input))
+}
